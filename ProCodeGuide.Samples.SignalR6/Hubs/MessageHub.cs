d80a160249784b4eff47120d392ace9152feffa5
Sanjay <support@procodeguide.com>
2022-03-29 10:35:28 +0530

    Add project files.

---
+++ ProCodeGuide.Samples.SignalR6/Hubs/MessageHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ProCodeGuide.Samples.SignalR6.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            if (string.IsNullOrEmpty(user))
+                await Clients.All.SendAsync("ReceiveMessageHandler", message);
+            else
+                await Clients.User(user).SendAsync("ReceiveMessageHandler", message);
+        }
+    }
+}
